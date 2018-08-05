f2736e2acf80513806b24cb4c8f3485bb78f6100
neal.bharucha@msichicago.org <nealbharucha@yahoo.com>
2018-08-05 15:56:47 -0500

    working image slider on mobile as well.

--- js/officeTour.js
+++ js/officeTour.js
@@ -1,6 +1,8 @@
 let officeTourIdx = 0;
+let treatmentIdx = 0;
+
 function buttonClickOffice(className, idxChange) {
-    var x = document.getElementsByClassName(className);
+    const x = document.getElementsByClassName(className);
     x[officeTourIdx].style.display = "none";
     officeTourIdx += idxChange;
     if (officeTourIdx > 5) {
@@ -9,4 +11,16 @@ function buttonClickOffice(className, idxChange) {
         officeTourIdx = 5;
     }
     x[officeTourIdx].style.display = "block";
+}
+
+function buttonClickTreatment(className, idxChange) {
+    const x = document.getElementsByClassName(className);
+    x[treatmentIdx].style.display = "none";
+    treatmentIdx += idxChange;
+    if (treatmentIdx > 2) {
+        treatmentIdx = 0;
+    } else if (treatmentIdx < 0) {
+        treatmentIdx = 2;
+    }
+    x[treatmentIdx].style.display = "block";
 }
