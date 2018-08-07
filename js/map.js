7e660cdf4509b9a5963b4382f1872a93aeb069f2
Neal Bharucha <neal.barucha@msichicago.org>
2018-08-07 09:24:20 -0500

    Change initial zoom

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ function map_init() {
     const location = { lat: 42.048631, lng: -87.746566 };
     
     let map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 18,
+        zoom: 17,
         center: location
     });
 
